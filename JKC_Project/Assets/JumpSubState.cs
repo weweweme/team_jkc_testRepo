6f5e00e04b4f7073428f1c26b62aad6780e35878
Huiji_MacMini <gmlwl0408@gmail.com>
2023-05-30 19:26:38 +0900

    [Feat] Jump -> Dive

---
+++ JKC_Project/Assets/JumpSubState.cs
@@ -0,0 +1,25 @@
+using Literal;
+using UnityEngine;
+
+public class JumpSubState : StateMachineBehaviour
+{
+    private PlayerInput _playerInput;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _playerInput = animator.GetComponent<PlayerInput>();
+    }
+    
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_playerInput.IsDive)
+        {
+            Debug.Log(_playerInput.IsDive);
+            animator.SetBool(AnimLiteral.ISDIVING, true);
+        }
+    }
+    
+    override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    {
+        // animator.SetBool(AnimLiteral.ISJUMPING, false);
+    }
+}
