38b1638eed2443526c21b1c68df3f50e99780aae
SuperSoldier28 <anemone955@naver.com>
2023-06-02 16:31:56 +0900

    과일 대포 맵 수정
    1. 과일 추가
    
    2. 과일은 물리 구현 되지 않음

--- JKC_Project/Assets/Kimdoik_Working Pole/Legend_Hoop/Scripts_Legend/Axe.cs
+++ JKC_Project/Assets/Kimdoik_Working Pole/Legend_Hoop/Scripts_Legend/Axe.cs
@@ -44,7 +44,7 @@ public class Axe : MonoBehaviour
         transform.Rotate(Vector3.forward * _axeRotationSpeed * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
